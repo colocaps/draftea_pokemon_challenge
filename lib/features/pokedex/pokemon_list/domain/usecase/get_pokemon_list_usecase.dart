a1c3c8a81a471cf9e375625452196b8961bffd42
Esteban <colocaps@gmail.com>
2025-12-26 11:21:07 -0300

    added pokemon list usecase

---
+++ lib/features/pokedex/pokemon_list/domain/usecase/get_pokemon_list_usecase.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: one_member_abstracts
+
+import 'package:dartz/dartz.dart';
+import 'package:draftea_pokemon_challenge/core/exceptions/http_exceptions.dart';
+import 'package:draftea_pokemon_challenge/features/pokedex/pokemon_list/domain/model/pokemon_list_model.dart';
+import 'package:draftea_pokemon_challenge/features/pokedex/pokemon_list/domain/repository/pokemon_list_repository.dart';
+import 'package:injectable/injectable.dart';
+
+abstract class PokemonListUsecase {
+  Future<Either<HttpException, PokemonListModel>> call({
+    int? limit,
+    int? offset,
+  });
+}
+
+@Injectable(as: PokemonListUsecase)
+class GetPokemonListUsecaseImpl extends PokemonListUsecase {
+  GetPokemonListUsecaseImpl({required this.repository});
+
+  final PokemonListRepository repository;
+
+  @override
+  Future<Either<HttpException, PokemonListModel>> call({
+    int? limit,
+    int? offset,
+  }) {
+    return repository.getPokemonList(limit: limit, offset: offset);
+  }
+}
